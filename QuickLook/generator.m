152e58e4a03b1bb92509d463493537f14d15a3ba
C.W. Betts <computers57@hotmail.com>
2025-04-21 15:49:32 -0600

    Simplify GBQuickLookRender: only have one method

--- QuickLook/generator.m
+++ QuickLook/generator.m
@@ -1,15 +1,21 @@
-#include <QuickLook/QuickLook.h>
+#import <Foundation/Foundation.h>
 #include "get_image_for_rom.h"
 
 #if TARGET_OS_OSX
-#include <Cocoa/Cocoa.h>
+#include <QuickLook/QuickLook.h>
+#import <Cocoa/Cocoa.h>
+typedef NSImage TheImageType;
+#else
+#import <UIKit/UIKit.h>
+typedef UIImage TheImageType;
+#endif
 
 OSStatus GBQuickLookRender(CGContextRef cgContext, CFURLRef url, bool showBorder)
 {
     /* Load the template NSImages when generating the first thumbnail */
-    static NSImage *template = nil;
-    static NSImage *templateUniversal = nil;
-    static NSImage *templateColor = nil;
+    static TheImageType *template = nil;
+    static TheImageType *templateUniversal = nil;
+    static TheImageType *templateColor = nil;
     static NSBundle *bundle = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
@@ -18,9 +24,15 @@ OSStatus GBQuickLookRender(CGContextRef cgContext, CFURLRef url, bool showBorder
     if (showBorder) {
         static dispatch_once_t onceToken;
         dispatch_once(&onceToken, ^{
+#if TARGET_OS_OSX
             template = [bundle imageForResource:@"CartridgeTemplate"];
             templateUniversal = [bundle imageForResource:@"UniversalCartridgeTemplate"];
             templateColor = [bundle imageForResource:@"ColorCartridgeTemplate"];
+#else
+            template = [UIImage imageNamed:@"CartridgeTemplate" inBundle:bundle withConfiguration:nil];
+            templateUniversal = [UIImage imageNamed:@"UniversalCartridgeTemplate" inBundle:bundle withConfiguration:nil];
+            templateColor = [UIImage imageNamed:@"ColorCartridgeTemplate" inBundle:bundle withConfiguration:nil];
+#endif
         });
     }
     uint32_t bitmap[160*144];
@@ -51,20 +63,30 @@ OSStatus GBQuickLookRender(CGContextRef cgContext, CFURLRef url, bool showBorder
                                     true,
                                     renderingIntent);
     CGContextSetInterpolationQuality(cgContext, kCGInterpolationNone);
+#if TARGET_OS_OSX
+    [NSGraphicsContext saveGraphicsState];
     NSGraphicsContext *context = [NSGraphicsContext graphicsContextWithCGContext:cgContext flipped:false];
     [NSGraphicsContext setCurrentContext:context];
+#else
+    UIGraphicsPushContext(cgContext);
+#endif
     
     
     double ratio = CGBitmapContextGetWidth(cgContext) / 1024.0;
     
     /* Convert the screenshot to a magnified NSImage */
-    NSImage *screenshot = [[NSImage alloc] initWithCGImage:iref size:NSMakeSize(160, 144)];
+    TheImageType *screenshot;
+#if TARGET_OS_OSX
+    screenshot = [[NSImage alloc] initWithCGImage:iref size:NSMakeSize(160, 144)];
+#else
+    screenshot = [[UIImage alloc] initWithCGImage:iref];
+#endif
     /* Draw the screenshot */
     if (showBorder) {
-        [screenshot drawInRect:NSMakeRect(192 * ratio, 150 * ratio, 640 * ratio, 576 * ratio)];
+        [screenshot drawInRect:CGRectMake(192 * ratio, 150 * ratio, 640 * ratio, 576 * ratio)];
     }
     else {
-        [screenshot drawInRect:NSMakeRect(0, 0, 640, 576)];
+        [screenshot drawInRect:CGRectMake(0, 0, 640, 576)];
     }
     
     if (showBorder) {
@@ -73,7 +95,7 @@ OSStatus GBQuickLookRender(CGContextRef cgContext, CFURLRef url, bool showBorder
          - CGB cartridges are transparent
          - CGB cartridges that support DMG systems are black
          */
-        NSImage *effectiveTemplate = nil;
+        TheImageType *effectiveTemplate = nil;
         switch (cgbFlag) {
             case 0xC0:
                 effectiveTemplate = templateColor;
@@ -83,20 +105,27 @@ OSStatus GBQuickLookRender(CGContextRef cgContext, CFURLRef url, bool showBorder
                 break;
             default:
                 effectiveTemplate = template;
+                break;
         }
         
         CGContextSetInterpolationQuality(cgContext, kCGInterpolationMedium);
         /* Mask it with the template (The middle part of the template image is transparent) */
-        [effectiveTemplate drawInRect:(NSRect){{0, 0}, {CGBitmapContextGetWidth(cgContext), CGBitmapContextGetHeight(cgContext)}}];
+        [effectiveTemplate drawInRect:(CGRect){{0, 0}, {CGBitmapContextGetWidth(cgContext), CGBitmapContextGetHeight(cgContext)}}];
     }
     
     CGColorSpaceRelease(colorSpaceRef);
     CGDataProviderRelease(provider);
     CGImageRelease(iref);
+#if TARGET_OS_OSX
+    [NSGraphicsContext restoreGraphicsState];
+#else
+    UIGraphicsPopContext();
+#endif
     
     return noErr;
 }
 
+#if TARGET_OS_OSX
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
     @autoreleasepool {
@@ -124,102 +153,5 @@ OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thum
         CGContextRelease(cgContext);
         return -1;
     }
-}
-#else
-#include <UIKit/UIKit.h>
-
-OSStatus GBQuickLookRender(CGContextRef cgContext, CFURLRef url, bool showBorder)
-{
-    /* Load the template UIImages when generating the first thumbnail */
-    static UIImage *template = nil;
-    static UIImage *templateUniversal = nil;
-    static UIImage *templateColor = nil;
-    static NSBundle *bundle = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        bundle = [NSBundle bundleForClass:NSClassFromString(@"GBPanel")];
-    });
-    if (showBorder) {
-        static dispatch_once_t onceToken;
-        dispatch_once(&onceToken, ^{
-            template = [UIImage imageNamed:@"CartridgeTemplate" inBundle:bundle withConfiguration:nil];
-            templateUniversal = [UIImage imageNamed:@"UniversalCartridgeTemplate" inBundle:bundle withConfiguration:nil];
-            templateColor = [UIImage imageNamed:@"ColorCartridgeTemplate" inBundle:bundle withConfiguration:nil];
-        });
-    }
-    uint32_t bitmap[160*144];
-    uint8_t cgbFlag = 0;
-
-    /* The cgb_boot_fast boot ROM skips the boot animation */
-    if (get_image_for_rom([(__bridge NSURL *)url fileSystemRepresentation],
-                          [[bundle URLForResource:@"cgb_boot_fast" withExtension:@"bin"] fileSystemRepresentation],
-                          bitmap, &cgbFlag)) {
-        return -1;
-    }
-    
-    /* Convert the screenshot to a CGImageRef */
-    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, bitmap, sizeof(bitmap), NULL);
-    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
-    CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault | kCGImageAlphaNoneSkipLast;
-    CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
-    
-    CGImageRef iref = CGImageCreate(160,
-                                    144,
-                                    8,
-                                    32,
-                                    4 * 160,
-                                    colorSpaceRef,
-                                    bitmapInfo,
-                                    provider,
-                                    NULL,
-                                    true,
-                                    renderingIntent);
-    CGContextSetInterpolationQuality(cgContext, kCGInterpolationNone);
-    UIGraphicsPushContext(cgContext);
-
-    
-    
-    double ratio = CGBitmapContextGetWidth(cgContext) / 1024.0;
-
-    /* Convert the screenshot to a magnified NSImage */
-    UIImage *screenshot = [[UIImage alloc] initWithCGImage:iref];
-    /* Draw the screenshot */
-    if (showBorder) {
-        [screenshot drawInRect:CGRectMake(192 * ratio, 150 * ratio, 640 * ratio, 576 * ratio)];
-    }
-    else {
-        [screenshot drawInRect:CGRectMake(0, 0, 640, 576)];
-    }
-    
-    if (showBorder) {
-        /* Use the CGB flag to determine the cartridge "look":
-         - DMG cartridges are grey
-         - CGB cartridges are transparent
-         - CGB cartridges that support DMG systems are black
-         */
-        UIImage *effectiveTemplate = nil;
-        switch (cgbFlag) {
-            case 0xC0:
-                effectiveTemplate = templateColor;
-                break;
-            case 0x80:
-                effectiveTemplate = templateUniversal;
-                break;
-            default:
-                effectiveTemplate = template;
-                break;
-        }
-        
-        CGContextSetInterpolationQuality(cgContext, kCGInterpolationMedium);
-        /* Mask it with the template (The middle part of the template image is transparent) */
-        [effectiveTemplate drawInRect:(CGRect){{0, 0}, {CGBitmapContextGetWidth(cgContext), CGBitmapContextGetHeight(cgContext)}}];
-    }
-    
-    CGColorSpaceRelease(colorSpaceRef);
-    CGDataProviderRelease(provider);
-    CGImageRelease(iref);
-    UIGraphicsPopContext();
-    
-    return noErr;
 }
 #endif
