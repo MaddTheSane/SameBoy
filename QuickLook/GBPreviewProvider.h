c0cde7f39c7b0e1348b8a22c417bfc86876be6d7
C.W. Betts <computers57@hotmail.com>
2025-07-27 02:14:02 -0600

    Xcode poke.

--- QuickLook/GBPreviewProvider.h
+++ QuickLook/GBPreviewProvider.h
@@ -1,9 +1,14 @@
 #import <Foundation/Foundation.h>
 #import <QuickLookThumbnailing/QuickLookThumbnailing.h>
-#import <QuickLookUI/QuickLookUI.h>
 #if TARGET_OS_OSX
+#import <QuickLookUI/QuickLookUI.h>
 #import <Cocoa/Cocoa.h>
 #import <Quartz/Quartz.h>
+#else
+#import <QuickLook/QLPreviewProvider.h>
+#import <QuickLook/QLPreviewingController.h>
+#import <QuickLook/QLPreviewReply.h>
+#import <QuickLook/QLFilePreviewRequest.h>
 #endif
 
 API_AVAILABLE(macos(12.0))
